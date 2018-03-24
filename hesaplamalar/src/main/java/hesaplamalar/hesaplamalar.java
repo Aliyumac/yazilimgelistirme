218d298e2f5993bfb66ba85c4fb3d201c1c18a12
bprlab <bprlab@10.201.35.150>
2018-03-24 14:23:25 +0300

    New project

---
+++ hesaplamalar/src/main/java/hesaplamalar/hesaplamalar.java
@@ -0,0 +1,16 @@
+package hesaplamalar;
+
+public class hesaplamalar {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=10;
+		int toplam=0;
+		toplam=a+b;
+		
+		System.out.println(toplam);
+
+	}
+
+}
